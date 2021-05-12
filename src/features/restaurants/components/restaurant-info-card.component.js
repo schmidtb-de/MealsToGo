a5b842e35e6430803e6e8162f353fe89efd60fe1
schmidtb-de <benny@schmidtb.de>
2021-05-12 16:22:07 +0200

    theming

---
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components/native";
+import { Card } from "react-native-paper";
+
+const Title = styled.Text`
+  padding: 16px;
+  color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+const RestaurantCard = styled(Card)`
+  background-color: white;
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: 20px;
+  background-color: white;
+`;
+
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon,
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    adress = "asdress",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily = "false",
+  } = restaurant;
+
+  return (
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Title>{name}</Title>
+    </RestaurantCard>
+  );
+};
